7f668e6023fca698ba66751623b6990579c05e15
tiw.exe <youvipy@gmail.com>
2026-09-01 22:19:23 +0700

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
+const CACHE = 'fx991cw-v5';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('fx991cw-v3').then(c => c.addAll(['./', './index.html', './manifest.json']))
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
